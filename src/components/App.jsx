14b8df4fdc2eee6fc5d30788f93cb63aa220b05f
amld24 <amld24@student.bth.se>
2025-10-16 17:23:17 +1000

    added sockets

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import FetchData from '../DataFetcher.jsx'
-import '../App.css'
 import Add from'./Add.jsx'
 import Edit from'./Update.jsx'
+import '../App.css'
 
 function Home() {
   return (
@@ -19,12 +19,14 @@ function App() {
       <nav className='nav-container'>
         <Link to="/">Home</Link> |{" "}
         <Link to="/add">Add</Link>
+        <Link to="/editor">Editor</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/add" element={<Add />} />
         <Route path="/update/:id" element={<Edit />} />
+        <Route path="/editor/:id" element={<Editor />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/TextEditor.jsx
@@ -0,0 +1,10 @@
+import FETCH_URI from '../utils.js';
+import { useState, useEffect } from 'react'
+import { useParams, useNavigate } from "react-router";
+import '../App.css'
+
+function Editor() {
+  
+}
+
+export default Editor
