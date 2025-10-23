142b5f9a716a131fc26d89ac3185d9f5215348d0
ShirinAjou <ajoushirin@gmail.com>
2025-10-23 11:47:33 +0200

    working on azure

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-let FETCH_URL = "https://jsramverk-shirin-hsfqftftd8b6d9fn.northeurope-01.azurewebsites.net";
+let FETCH_URL = "https://azure-shiajo-afa4fsg4a6h4beba.northeurope-01.azurewebsites.net/";
 if (location.host === "localhost") {
     FETCH_URL = "http://localhost:8080";
 }
