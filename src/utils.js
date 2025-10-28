8f54356b824bd763db6fb1ed436363a558c048ca
amld24 <amld24@student.bth.se>
2025-10-28 17:14:25 +1000

    .

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 let FETCH_URL = "https://azure-shiajo-afa4fsg4a6h4beba.northeurope-01.azurewebsites.net";
-if (location.host === "localhost") {
-    FETCH_URL = "http://localhost:8080";
-}
+// if (location.host === "localhost") {
+//     FETCH_URL = "http://localhost:8080";
+// }
 export default FETCH_URL;
