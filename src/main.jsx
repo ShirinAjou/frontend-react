8649ecf2c84de34d37917313d8bb2ec4d51515a3
amld24 <amld24@student.bth.se>
2025-10-29 21:55:43 +1000

    basename fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,16 @@ import App from './components/App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter basename="/frontend-react">
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>
 );
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <HashRouter basename="/frontend-react">
+//       <App />
+//     </HashRouter>
+//   </StrictMode>
+// );
